bc90c0ff45bb6d560028b7d699958517d66d50f8
Ben Y <GitYBenY-a4h1@mailnicks.org>
2018-07-22 12:40:08 +0300

    takes text and returns clacks (terminal only)

---
+++ clack.py
@@ -0,0 +1,73 @@
+# all the code for taking text and returning clacks
+
+def clack(txt):
+    """
+    Takes text and returns clacks
+    """
+    ans = ""
+    for ii in range(len(txt)):
+        ans += make8(ord(txt[ii])) # gives str of binary
+
+    ans = formatter(ans)
+
+    return ans
+
+
+def make8(int):
+    """
+    Takes an integer and returns its 8-bit value as a string
+    """
+    # gets binary of int without '0b' prefix
+    # zfill fills with 0 in front until length is 8
+    return str(bin(int)[2:].zfill(8))
+
+
+def formatter(txt):
+    """
+    Takes text of 8-bit ascii and formats to clacks formatting
+    """
+    # append null char if chars are not even
+    while (len(txt) /8) %2 != 0:
+        txt += "00000000"
+
+    txt = visualChange(txt)
+
+    # formats into clacks formatting 
+    clack = ""
+    for i in range(len(txt)):
+        
+        # skips the first loop
+        if i != 0:
+            clack += " "
+
+        # split to rows of 4 
+        if i %4 == 0:
+            clack += "\n"
+
+        # split into 4x4
+        if (i /8) %2 == 0:
+            clack += "\n"
+
+        clack += txt[i]
+
+
+
+    
+    return clack
+
+
+def visualChange(txt):
+    """
+    Takes text of 8-bit ascii and changes 0 to ● and 1 to ○ 
+    """
+    asDots = ""
+    for ii in range(len(txt)):
+        if txt[ii] == '0':
+            asDots += "●"
+        else:
+            asDots += "○"
+
+    return asDots
+
+
+
---
+++ main.py
@@ -0,0 +1,16 @@
+""" TODO
+[X] convert from text to clacks
+[ ] convert from clacks to text
+[ ] take txt and return results in txt
+"""
+
+# REMEMBER: Make this work with ascii
+# clack.JSON is only there to remind
+
+import clack
+
+user = input('Type Here:\n')
+
+print(clack.clack(user))
+
+
