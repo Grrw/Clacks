327847f2437a045ce7e77672e8e5591366a05176
Ben Y <GitYBenY-a4h1@mailnicks.org>
2018-07-22 16:17:12 +0300

    takes clacks and returns text

--- back.py
+++ back.py
@@ -4,35 +4,57 @@ def back(clack):
     """
     Takes clacks and returns text
     """
-    txt = ''
-    pass
+    clack = deformatter(clack)
+
+    ordList = makeBase10(clack)
+
+    # charList
+    charList = ''
+    for ii in range(len(ordList)):
+        charList += chr(int(ordList[ii], 2))
+
+    return charList
 
 
-def makeBase10(givenInt):
+def makeBase10(givenStr):
     """
-    Takes an 8-bit value as a string and returns it as an integer
+    Takes a string of 1s and 0s seperated by commas.
+    Returns it as a list of strings using ord(), splitting at the commas
     """
-    pass
+    strAsList = givenStr.split(',')
+
+    return strAsList
 
 
 def deformatter(clack):
     """
-    Takes clacks, removes whitespaces and replaces newlines with commas 
+    Takes clacks, removes whitespaces and adds commas every 8 characters
     """
-    result = ''
+    clack = visualChange(clack)
+
+    # adds commas every 8 chars
+    tmpClack = ''
     for ii in range(len(clack)):
+        if ii %8 == 0:
+            tmpClack += ','
+        tmpClack += clack[ii]
+    clack = tmpClack[1:] # remove leading comma
 
-        if clack[ii] == '\n':
-            result += ','
-
-        if clack[ii] != ' ':
-            result += clack[ii]
-    
-    return result
+    return clack
 
 
 def visualChange(txt):
-    pass
+    """
+    Takes text of clacks, changes ● to 0 and ○ to 1 and removes whitespace
+    """
+    asNums = ''
+    for ii in range(len(txt)):
+        if txt[ii] == '●':
+            asNums += "0"
+        elif txt[ii] == '○':
+            asNums += "1"
+
+    return asNums
 
 
 
