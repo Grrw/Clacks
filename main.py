8a0f6b913a6e99c8599366e06397dd85da168f7d
Ben Y <GitYBenY-a4h1@mailnicks.org>
2018-07-22 16:13:17 +0300

    full encode / decode functionality

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-""" TODO
-[X] convert from text to clacks
-[ ] convert from clacks to text
-[ ] take txt and return results in txt
-"""
-
-# REMEMBER: Make this work with ascii
-# clack.JSON is only there to remind
+# main file
 
 import clack, back
 selLoop = True
@@ -14,20 +7,20 @@ while selLoop:
 
     if selection == 'e':
         user = input('Input Message:\n')
-        print(clack.clack(user))
+        print('\nResult:' +clack.clack(user))
         selLoop = False
 
     elif selection == 'd':
         user = ''
         print("Input Message:\n(type 'done' on a new line to stop)")
+        
         while True:
             tmpTxt = input()
             if tmpTxt == 'done':
                 break
             user += tmpTxt
 
-        print(back.deformatter(user))
-        # print(back.back(user))
+        print('\nResult:\n' +back.back(user))
         selLoop = False
 
     elif selection == 'q':
