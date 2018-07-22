5f78669a79cc40880e4a0060afc0f26557fdd319
Ben Y <GitYBenY-a4h1@mailnicks.org>
2018-07-22 20:04:53 +0300

    prepare for txt files

--- main.py
+++ main.py
@@ -1,30 +1,44 @@
 # main file
 
+import sys
 import clack, back
-selLoop = True
-while selLoop:
-    selection = input("Press 'q' to Quit.\nEncode or Decode? (e/d)\n")
 
-    if selection == 'e':
-        user = input('Input Message:\n')
-        print('\nResult:' +clack.clack(user))
-        selLoop = False
+def main():
+    filename = sys.argv[-1]
 
-    elif selection == 'd':
-        user = ''
-        print("Input Message:\n(type 'done' on a new line to stop)")
-        
-        while True:
-            tmpTxt = input()
-            if tmpTxt == 'done':
-                break
-            user += tmpTxt
+    if filename == 'main.py':
+        oldMain()
 
-        print('\nResult:\n' +back.back(user))
-        selLoop = False
+    else:
+        print('TBD')
 
-    elif selection == 'q':
-        selLoop = False
+def oldMain():
+    selLoop = True
+    while selLoop:
+        selection = input("Press 'q' to Quit.\nEncode or Decode? (e/d)\n")
 
+        if selection == 'e':
+            user = input('Input Message:\n')
+            print('\nResult:' +clack.clack(user))
+            selLoop = False
+
+        elif selection == 'd':
+            user = ''
+            print("Input Message:\n(type 'done' on a new line to stop)")
+            
+            while True:
+                tmpTxt = input()
+                if tmpTxt == 'done':
+                    break
+                user += tmpTxt
+
+            print('\nResult:\n' + '"' +back.back(user)+ '"')
+            selLoop = False
+
+        elif selection == 'q':
+            selLoop = False
+
+
+main()
 
 
